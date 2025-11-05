2dd1014a5ff83053d4a56d2ceca5c8e41f33c414
Prateek Arora <prateekarorav2@gmail.com>
2025-11-04 21:39:11 -0800

    auto ip discovery leftover

--- mobile/leanframe_uploader_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile/leanframe_uploader_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,11 +5,13 @@
 import FlutterMacOS
 import Foundation
 
+import bonsoir_darwin
 import file_picker
 import mobile_scanner
 import photo_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  SwiftBonsoirPlugin.register(with: registry.registrar(forPlugin: "SwiftBonsoirPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   PhotoManagerPlugin.register(with: registry.registrar(forPlugin: "PhotoManagerPlugin"))
